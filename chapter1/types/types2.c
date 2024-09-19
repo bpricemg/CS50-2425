b03ca560e6014e3236ff6fbd83679923c1fc91b4
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-09-19 13:53:50 -0400

    Updated 9/19

---
+++ chapter1/types/types2.c
@@ -0,0 +1,23 @@
+/**
+ * This program will create variables, assigns values,
+ * and prints.
+ *
+ * Brandon Price
+ * CS50 AP
+ * Mount Greylock
+ *
+ * 9/19/2024
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    // Initalize an integer variable called x with the value 13
+    int x = 13;
+
+    // Initialize an integer variable called y with the value 6
+    int y = 6;
+
+    // Print out the current value of x
+    printf("The value of x is %i and the value of y is %i.\n", x, y);
+}
---
+++ chapter1/types/types3.c
@@ -0,0 +1,25 @@
+/**
+ * This program will create variables, assigns values,
+ * and prints.
+ *
+ * Brandon Price
+ * CS50 AP
+ * Mount Greylock
+ *
+ * 9/19/2024
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    // Initalize x and y
+    int x = 13;
+    int y = 6;
+
+    // Update the values of x and y
+    x = 5;
+    y = y + 4;
+
+    // Print out the current value of x
+    printf("The value of x is %i and the value of y is %i.\n", x, y);
+}
