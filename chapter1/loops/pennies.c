5e16cbb197337b1984c63cda402f6f784c27b488
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-09-27 09:12:06 -0400

    Updated 9/27

---
+++ chapter1/loops/pennies.c
@@ -0,0 +1,18 @@
+/**
+ * Calculate the number of pennies someone would have if
+ * their change doubled every day for a month
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get starting change in pennies 
+
+    // Get the number of days in the month
+
+    // Double the number of pennies each day for a month
+
+    // Print the result
+}
