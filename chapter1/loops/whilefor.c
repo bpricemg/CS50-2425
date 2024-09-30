6478a17580e4afd730dbfaebbfeee268d53d8a25
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-09-30 09:13:36 -0400

    Updated 9/30

---
+++ chapter1/loops/whilefor.c
@@ -0,0 +1,13 @@
+/**
+ * While loop vs for loop
+ */
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Use a while loop to say "hello, world" 5 times
+    int counter = 0;
+    while ()
+}
