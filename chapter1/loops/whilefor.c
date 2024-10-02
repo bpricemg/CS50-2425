deff7495888856325cefa11dac8c98ef038aacd5
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-10-02 13:59:11 -0400

    Updated 10/2

--- chapter1/loops/whilefor.c
+++ chapter1/loops/whilefor.c
@@ -7,7 +7,23 @@
 
 int main(void)
 {
+    /**
     // Use a while loop to say "hello, world" 5 times
     int counter = 0;
-    while ()
+    while (counter < 5)
+    {
+        printf("hello, world\n");
+
+        // Increase the counter by 1
+        // counter = counter + 1;
+        // counter += 1;
+        counter++;
+    }
+    */
+
+   // Use a for loop instead, wich is generally better
+   for (int counter = 0; counter < 5; counter++)
+   {
+        printf("hello, world\n");
+   }
 }
