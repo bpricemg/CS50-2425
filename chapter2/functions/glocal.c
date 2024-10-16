182d87dffb1161526c37b2b5d2156d662b03bdfe
bpricemg <43038187+bpricemg@users.noreply.github.com>
2024-10-16 14:27:59 -0400

    Updated 10/16

--- chapter2/functions/glocal.c
+++ chapter2/functions/glocal.c
@@ -1,22 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// TODO: function prototype to fix
-void add_5(int x);
+// function prototype to fix
+int add_5(int x);
 
 int main(void)
 {
     // input from user
     int x = get_int("Enter some integer value: ");
     printf("the value of x before adding 5 is %d\n", x);
-    
-    // TODO: fix this so that the value of x is updated
-    add_5(x);
-    printf("the value of x after adding 5 is %d\n", x);
+
+    // fix this so that the value of x is updated
+    printf("the value of x after adding 5 is %d\n", add_5(x));
 }
 
-// TODO: function definition to fix
-void add_5(int x)
+// function definition to fix
+int add_5(int x)
 {
-    x = x + 5;
+    return x + 5;
 }
