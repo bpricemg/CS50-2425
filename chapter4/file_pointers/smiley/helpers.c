24770f1eed708c496bee3bd682f44d192baae34d
bpricemg <43038187+bpricemg@users.noreply.github.com>
2025-01-22 14:27:51 -0500

    Updated 1/22

--- chapter4/file_pointers/smiley/helpers.c
+++ chapter4/file_pointers/smiley/helpers.c
@@ -2,6 +2,26 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to red
-    
+    // For each row in the height
+    for (int i = 0; i < height; i++)
+    {
+        // For each column in the width
+        for (int j = 0; j < width; j++)
+        {
+            // Change all black pixels to bright yellow
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed = 0xff;
+                image[i][j].rgbtGreen = 0xff;
+            }
+
+            // Change all white pixels to teal
+            if (image[i][j].rgbtRed == 0xff && image[i][j].rgbtGreen == 0xff && image[i][j].rgbtBlue == 0xff)
+            {
+                image[i][j].rgbtRed = 0x4b;
+                image[i][j].rgbtGreen = 0xc8;
+                image[i][j].rgbtBlue = 0xaf;
+            }
+        }
+    }
 }
