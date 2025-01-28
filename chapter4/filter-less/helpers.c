96d8ff7703ef16cb8d819149d90812be14778aa5
bpricemg <43038187+bpricemg@users.noreply.github.com>
2025-01-28 08:05:25 -0500

    Updated 1/28

--- chapter4/filter-less/helpers.c
+++ chapter4/filter-less/helpers.c
@@ -13,6 +13,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // For each pixel
+        // Calculate each new color value using the sepia formula
+        // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+        // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+        // sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+
+        // Be sure the result is an integer between 0 and 255, inclusive
     return;
 }
 
